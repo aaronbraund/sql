fdc26eb3e6f511624e2ac068c4ded94d3192bdb1
aaronbraund <158840972+aaronbraund@users.noreply.github.com>
2024-02-24 12:10:48 +0000

    Update bq_joins.sql

--- bq_joins.sql
+++ bq_joins.sql
@@ -1,4 +1,4 @@
 SELECT e.name AS employee,e.role AS employee_role, d.name AS department
-FROM plenary-anagram-413215.empoyee_id.employees AS e
-LEFT JOIN plenary-anagram-413215.empoyee_id.departments AS d
+FROM plenary-anagram-413215.employee_id.employees AS e
+LEFT JOIN plenary-anagram-413215.employee_id.departments AS d
 ON e.department_id = d.department_id
